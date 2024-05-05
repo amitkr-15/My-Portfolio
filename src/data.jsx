2723fd26806bea1f553628c1faa5bb8c4cd217a3
Amit Kumar <113462014+amitkr-15@users.noreply.github.com>
2024-05-05 10:16:37 +0530

    add manual data to the portfolio for project, skills & testimonials

---
+++ src/data.jsx
@@ -0,0 +1,28 @@
+export  const skills = [
+  "JavaScript",
+  "ReactJs",
+  "CSS",
+  "Tailwind",
+  "Bootstrap",
+  "Material UI",
+];
+
+export const projects = [
+  {
+    title: "Myntra React Clone",
+    subtitle: "MERN Stack",
+    description:
+      "Myntra UI/UX Clone using JavaScript React library . Here redux , router ,fetching , data from backend , hook concept are used to build this project",
+    image: "./myntra.png",
+    link: "https://github.com/amitkr-15/Myntra-UI-Project",
+  },
+ 
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./1243111.png",
+    link: "https://pythonbootcamp.com",
+  },
+];
